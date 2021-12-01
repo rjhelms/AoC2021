502fd6b6e1b114f198fa2b73367bfdbafdc76b33
Rob Hailman <robhailman@gmail.com>
2021-12-01 12:13:35 -0500

    Complete day 1

---
+++ 1a.py
@@ -0,0 +1,19 @@
+from os import path
+
+INPUT = path.join("input", "1.txt")
+
+def main():
+    measurements = []
+    with open(INPUT, 'r') as in_file:
+        for line in in_file:
+            measurements.append(int(line))
+    
+    count = 0
+    for i in range(1, len(measurements)):
+        if measurements[i] > measurements[i-1]:
+            count += 1
+    
+    print(count)
+
+if __name__ == "__main__":
+    main()

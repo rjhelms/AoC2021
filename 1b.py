f80aaa8045655e8441d89773ca562b5143cbd6af
Rob Hailman <robhailman@gmail.com>
2021-12-02 00:12:14 -0500

    Format w/ black

--- 1b.py
+++ 1b.py
@@ -2,22 +2,24 @@ from os import path
 
 INPUT = path.join("input", "1.txt")
 
+
 def main():
     measurements = []
     window = []
-    with open(INPUT, 'r') as in_file:
+    with open(INPUT, "r") as in_file:
         for line in in_file:
             measurements.append(int(line))
-    
+
     for i in range(2, len(measurements)):
-        window.append(sum(measurements[i-2:i+1]))
-    
+        window.append(sum(measurements[i - 2 : i + 1]))
+
     count = 0
     for i in range(1, len(window)):
-        if window[i] > window[i-1]:
+        if window[i] > window[i - 1]:
             count += 1
-    
+
     print(count)
+
 
 if __name__ == "__main__":
     main()
--- 2b.py
+++ 2b.py
@@ -2,11 +2,12 @@ from os import path
 
 INPUT = path.join("input", "2.txt")
 
+
 def main():
     x_pos = 0
     y_pos = 0
     aim = 0
-    with open(INPUT, 'r') as in_file:
+    with open(INPUT, "r") as in_file:
         for line in in_file:
             if line.split()[0] == "forward":
                 x_pos += int(line.split()[1])
@@ -14,9 +15,10 @@ def main():
             elif line.split()[0] == "up":
                 aim -= int(line.split()[1])
             elif line.split()[0] == "down":
-                aim += int(line.split()[1])               
-    
+                aim += int(line.split()[1])
+
     print(x_pos * y_pos)
+
 
 if __name__ == "__main__":
     main()
